8fa07c26118c306ed91e268902af8eeda3f723a5
gabrial-alves <g29alves@gmail.com>
2024-05-10 11:55:32 -0300

    primeiro commit projeto

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
+import React from 'react'
 
-export default function Home() {
+export default function teste() {
   return (
     <div className="w-[100%] top-0 h-[12vh] bg-[--cor-desktop] shandow-md">
       <div className='flex items-center justify-between w-[80%] mx-auto h-[100%] '>
         <h1 className='flex-[0.6] cursor-pointer text-[25px] text-white font-bold'>
         AGENDE
-        <span className='text-[--cor-logo]'>SAÚDE</span>
+        <span className='text-#2EB3E4'>SAÚDE</span>
         </h1>
 
         <div className='nav-link'>Home</div>
@@ -18,4 +17,3 @@ export default function Home() {
     </div>
   )
 }
-
